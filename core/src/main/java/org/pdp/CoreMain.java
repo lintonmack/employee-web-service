c67dc4ae54be4e7acdaf4c54f7d4c2f1e0cbc2ce
Linton Mackereth <lintonmackereth@gmail.com>
2023-05-16 10:08:20 +0100

    Renamed main classes for better readability & context

---
+++ core/src/main/java/org/pdp/CoreMain.java
@@ -0,0 +1,15 @@
+package org.pdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class CoreMain {
+    @SpringBootApplication
+    public class CoreMaine
+    {
+        public static void main( String[] args )
+        {
+            SpringApplication.run(CoreMain.class, args);
+        }
+    }
+}
