873baa66df59c8a56669606d83eafbea5c0c5ff9
Linton Mackereth <lintonmackereth@gmail.com>
2023-05-16 10:11:16 +0100

    Refactored controller to return responses & added route to getEmployee by id

--- api/src/main/java/org/pdp/controllers/EmployeeController.java
+++ api/src/main/java/org/pdp/controllers/EmployeeController.java
@@ -4,12 +4,17 @@ import org.pdp.model.Employee;
 import org.pdp.service.EmployeeService;
 import org.pdp.util.ViewNames;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@ResponseBody
 public class EmployeeController {
 
     private final EmployeeService employeeService;
@@ -20,14 +25,36 @@ public class EmployeeController {
     }
 
     @GetMapping(ViewNames.EMPLOYEES)
-    public List<Employee> getAllEmployees() {
+    public ResponseEntity<List<Employee>> getAllEmployees() {
         List<Employee> employees = employeeService.getAllEmployees();
-        return employees;
+        if (employees != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(employees);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
 
-    @GetMapping("/")
-    public List<Employee> home() {
-        List<Employee> employees = employeeService.getAllEmployees();
-        return employees;
+    @GetMapping(ViewNames.EMPLOYEE)
+    public ResponseEntity<Employee> getEmployee(@RequestParam String id) {
+        Employee employee = employeeService.getEmployee(id);
+        if (employee != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(employee);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
+
+    // ToDo: Add Unit tests for the above an look to add an integration test
+    // ToDo: Add functionality to get single employee by id from the records
+    // ToDo: Add tests to support
+    // ToDo: Add remainder of CRUD methods and add tests to support
+    // ToDo: Add MongoDB connection
+    // ToDo: Add Integration tests
+    // ToDo: Add API key integration
+    // ToDo: Add React integration
+
+
+//    @GetMapping("/")
+//    public List<Employee> home() {
+//        List<Employee> employees = employeeService.getAllEmployees();
+//        return employees;
+//    }
 }
--- api/src/main/java/org/pdp/util/ViewNames.java
+++ api/src/main/java/org/pdp/util/ViewNames.java
@@ -3,6 +3,7 @@ package org.pdp.util;
 public class ViewNames {
     public static final String HOME = "home";
     public static final String EMPLOYEES = "employees";
+    public static final String EMPLOYEE = "employee";
 
     private ViewNames(){};
 }
